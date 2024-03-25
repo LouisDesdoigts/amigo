42b9a64100cccbc4d42bdfca39a0e698b8f442fe
Louis Desdoigts <ldes9555@uni.sydney.edu.au>
2024-03-25 13:19:15 +1100

    Adding some loose code for CNNs construction

--- src/amigo/CNN.py
+++ src/amigo/CNN.py
@@ -115,3 +115,138 @@ class ConvBFE(eqx.Module):
     def apply_array(self, x):
         """This only exists to match PolyBFE methods"""
         return self(x)
+
+
+# import jax
+# import jax.numpy as np
+# import jax.random as jr
+# import equinox as eqx
+# from amigo.CNN import ConvBFE, Expand, Squeeze, calc_rfield
+
+# kernel_size = 3
+# oversample = model.optics.oversample
+# N = kernel_size * oversample
+
+
+# Conv2d = lambda **kwargs: eqx.nn.Conv2d(
+#     **kwargs,
+#     kernel_size=kernel_size,
+# )
+
+# key = jr.PRNGKey(0)
+# subkeys = jr.split(key, 100)
+
+# widths = [
+#     16,
+#     16,
+#     4,
+#     4
+# ]
+
+# layers = [
+#     # Expand(), # Dont need expand, handled by the image2grads fn
+#     Conv2d(
+#         in_channels=4,
+#         out_channels=widths[0],
+#         stride=(1, 1),
+#         padding=1,
+#         key=subkeys[0],
+#     ),
+#     jax.nn.relu,
+#     Conv2d(
+#         in_channels=widths[0],
+#         out_channels=widths[1],
+#         stride=(2, 2),
+#         padding=1,
+#         key=subkeys[1],
+#     ),
+#     jax.nn.relu,
+#     Conv2d(
+#         in_channels=widths[1],
+#         out_channels=widths[2],
+#         stride=(1, 1),
+#         padding=1,
+#         key=subkeys[2],
+#     ),
+#     jax.nn.relu,
+#     Conv2d(
+#         in_channels=widths[2],
+#         out_channels=widths[3],
+#         stride=(2, 2),
+#         padding=1,
+#         key=subkeys[3],
+#     ),
+#     jax.nn.relu,
+#     Conv2d(
+#         in_channels=widths[3],
+#         out_channels=1,
+#         dilation=(2, 2),
+#         padding=2,
+#         key=subkeys[4],
+#     ),
+#     Squeeze(),
+# ]
+# print(f"Field of Regard: {calc_rfield(layers)}")
+
+
+# ramp = model_fn(model, base_exposures[0], to_BFE=True)
+# # x =
+# x = np.zeros((4,) + ramp.shape[1:])
+# # x = jr.normal(jr.PRNGKey(1), x.shape)
+# print(f"Input shape: {x.shape}")
+# print()
+
+# for layer in layers:
+#     x = layer(x)
+#     if isinstance(layer, (eqx.nn.Conv, eqx.nn.Pool, Squeeze, Expand)):
+#         string = "\n" + str(type(layer)).split(".")[-1][:-2] + f" -> {x.shape}"
+#         if isinstance(layer, eqx.nn.Conv):
+#             string += (
+#                 # f"  Pad: {layer.padding[0][0]}\n"
+#                 f"\n  Stride: {layer.stride[0]}\n"
+#                 f"  Dilation: {layer.dilation[0]}"
+#             )
+#         print(string)
+#         # print(f)
+
+
+# import jax.tree_util as jtu
+
+# def leaf_fn(leaf):
+#     if hasattr(leaf, 'size'):
+#         return leaf.size
+#     else:
+#         return 0
+
+# vals = np.array(jtu.tree_leaves(jtu.tree_map(leaf_fn, layers))).sum()
+# vals
+
+
+# k = (model.optics.psf_npixels - 80) // 2
+
+# convBFE = ConvBFE(layers, oversample, pad=k)
+# # model = model.set("BFE", convBFE)
+
+# psf = ramp[-1]
+# im1 = dlu.downsample(psf, oversample, mean=False)[k:-k, k:-k]
+# im2 = convBFE(psf)[k:-k, k:-k]
+# bleeding = im1 - im2
+# print()
+# print(im1.sum(), im2.sum())
+# print(im1.sum() / im2.sum())
+# print(bleeding.sum())
+
+# plt.figure(figsize=(15, 4))
+# plt.subplot(1, 3, 1)
+# plt.imshow(im1)
+# plt.colorbar()
+
+# plt.subplot(1, 3, 2)
+# plt.imshow(im2, cmap="inferno")
+# plt.colorbar()
+
+# plt.subplot(1, 3, 3)
+# v = np.abs(bleeding).max()
+# plt.imshow(bleeding, cmap="seismic", vmin=-v, vmax=v)
+# plt.colorbar()
+# plt.show()
