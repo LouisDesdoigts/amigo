dfadff9fa9d89c2065317eb13360c98d72e3aae3
maxecharles <max.charles@sydney.edu.au>
2026-03-30 16:07:16 +1100

    removing numpy from imports

--- src/amigo/plotting.py
+++ src/amigo/plotting.py
@@ -4,7 +4,6 @@ import jax.scipy as jsp
 import dLux.utils as dlu
 import matplotlib.pyplot as plt
 from matplotlib import colormaps, colors
-import numpy as onp
 
 # from .stats import posterior
 
@@ -562,14 +561,14 @@ def _plot_param(ax, arr, param, start=0, end=-1, **kwargs):
             total = arr.shape[0]
             maxn = 100
             if total > maxn:
-                idxs = onp.random.choice(np.arange(total), size=maxn, replace=False)
+                key = jr.PRNGKey(0)  # or pass this in properly
+                idxs = jr.choice(key, total, shape=(maxn,), replace=False)
                 idxs = np.sort(idxs)
                 arr = arr[idxs, :]
                 epochs = epochs[idxs]
 
             ax.plot(epochs, arr, alpha=0.5, **kwargs)
             ax.set(ylabel="Log Distribution")
-
 
         case _:
             # print(f"No formatting function for {param}")
